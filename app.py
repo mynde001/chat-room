8e8ff2916042c310d67d4f7d1e94ad0b6db75545
Mindaugas <mindaugas.antanas.grigaitis@gmail.com>
2023-03-20 13:38:22 +0200

    Moved js to a separate file, added a disconnect message

--- app.py
+++ app.py
@@ -11,16 +11,22 @@ def main():
 
 # Initial connection, public announcement of connection
 # sid = Session ID
-@socketio.on("connecting")
+@socketio.on("connect")
 def on_connect():
     sid = request.sid
-    emit("connection_approved", sid, broadcast=True)
+    emit("user_connect", sid, broadcast=True)
 
 # Chat
 @socketio.on("messaging")
 def send_message(message):
     sid = request.sid
     emit("chat_message", {"id": sid, "message": message}, broadcast=True)
+
+# Disconnect
+@socketio.on("disconnect")
+def user_disconnect():
+    sid = request.sid
+    emit("user_disconnect", sid, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
